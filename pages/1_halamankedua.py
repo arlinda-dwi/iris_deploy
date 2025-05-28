0b2ed6d7b692c69df5b39f70f8c34e68c81e5976
arlinda-dwi <arlindabelajar44@students.unnes.ac.id>
2025-05-28 14:11:29 +0700

    Update 1_halamankedua.py

--- pages/1_halamankedua.py
+++ pages/1_halamankedua.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_tittle="Dashboard")
+st.set_page_config(page_tittle="Halaman Kedua")
 st.tittle("Tes")
-st.sidebar.header("Dashboard")
+st.sidebar.header("Halaman Kedua")
