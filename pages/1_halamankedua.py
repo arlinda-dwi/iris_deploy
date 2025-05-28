ad15e02ee992638cff8886fa0e7e03559e891b20
arlinda-dwi <arlindabelajar44@students.unnes.ac.id>
2025-05-28 14:37:30 +0700

    Delete pages directory

--- pages/1_halamankedua.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_tittle="Halaman Kedua")
-st.tittle("Tes")
-st.sidebar.header("Halaman Kedua")
--- pages/2_halamanketiga.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_tittle="Halaman Ketiga", page_icon"
-st.tittle("Tes")
-st.sidebar.header("Halaman Ketiga")
